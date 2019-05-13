23071363c3f7a790dcf92789a85b8c1d65519a8e
Irma Mesa <irmaamesa@me.com>
2019-05-13 19:12:50 -0400

    wrapped up auth, login, user models

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Learnings:
 - Creating views (EJS)
 - Using Bootstrap and FontAwesome for styling
 - Setting up the routes (/user, /registration, /login)
+
+### 5/13
+
+Learnings:
+
+- Created a user
+- Setup validation for Register and Login
+- Validation pass
+- Created new user instance
+- Ran into an ejslint error that pointed to register.js having an extra closing bracket :(
+- Initialized PassportJS
+- Handled login
+- Handled logout
+- Handled authentication via `auth.js` so that a logged out user cannot view dashboard
+- Learned a bit more about using partials
--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+// Passport config
+require("./config/passport")(passport);
 
 // DB config
 const db = require("./config/key").MongoURI;
@@ -16,6 +22,33 @@ mongoose
 // EJS middleware
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Express session middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Passport middleware (initalizing LocalStrategy  )
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash middleware
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //Routes
 app.use("/", require("./routes/index.js"));
